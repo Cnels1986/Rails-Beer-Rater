956395da24ea302dbcc1b76b26982ca9f395144f
Cnels1986 <CNels1986@gmail.com>
2018-02-23 12:54:09 -0500

    got user sign up working

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,6 @@ Location.create(name: 'Beer Mongers')
 Location.create(name: 'The Fridge')
 Location.create(name: 'First Post')
 
-User.create(name: 'Chris Nelson', email: 'Cnels1986@gmail.com')
-
 Beer.create(user_id: 1, name: 'Hop Hands', brewery_id: 1, location_id: 1, beer_type: 'Pale Ale', rating: 5)
 Beer.create(user_id: 1, name: 'Soft Sphere', brewery_id: 1, location_id: 1, beer_type: 'IPA', rating: 5)
 Beer.create(user_id: 1, name: 'Golden Monkey', brewery_id: 3, location_id: 3, beer_type: 'Belgian Tripel', rating: 5)
