40782600cffb8ffb9576bc734e0bf04e9fec6dd6
Cnels1986 <CNels1986@gmail.com>
2018-03-04 15:21:08 -0500

    fixed the brewery and location pages to display everything checked in from there

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,20 @@
+class LocationsController < ApplicationController
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    @beers = Beer.where(location_id: @location.id)
+  end
+
+  private
+
+    def location_params
+      params.require(:location).permit(:name)
+    end
+end
