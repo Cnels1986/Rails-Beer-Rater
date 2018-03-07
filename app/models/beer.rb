0e02db6af284b81d8d5f1dbe099c027744997514
Cnels1986 <CNels1986@gmail.com>
2018-03-07 15:35:45 -0500

    fixed some formatting, added image upload on the beer checkin page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,8 +3,19 @@ class Beer < ApplicationRecord
   belongs_to :brewery
   accepts_nested_attributes_for :brewery
   belongs_to :location
+  mount_uploader :picture, PictureUploader
   validates :name, presence: true
   validates :rating, presence: true
   validates_inclusion_of :rating, :in => 1..5
   default_scope -> { order(created_at: :desc) }
+  validate  :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
